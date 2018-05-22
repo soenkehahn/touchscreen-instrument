44789d9f4c3324a9ff19c1a81379b133d3499d57
Sönke Hahn <soenkehahn@gmail.com>
2018-05-22 12:45:20 -0400

    display x and y, not diffs

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,17 @@ use std::result::*;
 fn main_() -> Result<(), Error> {
     let fs = FsPool::default();
     let read = fs.read("/dev/input/mouse0", Default::default());
+    let mut x: i32 = 0;
+    let mut y: i32 = 0;
     for foo in read.wait() {
         let v: Bytes = foo?;
         if v.len() != 3 {
             panic!("expected: length of 3");
         }
-        let (x, y): (i8, i8) = unsafe { (transmute(v[1]), transmute(v[2])) };
-        print!("\rx: {:4}, y: {:4}", x, y);
+        let (x_diff, y_diff): (i8, i8) = unsafe { (transmute(v[1]), transmute(v[2])) };
+        x += x_diff as i32;
+        y += y_diff as i32;
+        print!("\r\x1b[Kx: {:4}, y: {:4}", x, y);
         let _ = stdout().flush();
     }
     Ok(())
