af1548509da862a76d5f51a5b324e1a05ce3f701
Sönke Hahn <soenkehahn@gmail.com>
2018-05-21 20:01:06 -0400

    parse mouse binary values

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 #![feature(type_ascription)]
 
+extern crate bytes;
 extern crate futures;
 extern crate futures_fs;
 
 use futures_fs::FsPool;
 use futures::stream::Stream;
+use bytes::Bytes;
+use std::io::*;
+use std::mem::*;
 
 fn main() {
     let fs = FsPool::default();
-    let read = fs.read("mouse.data", Default::default());
+    let read = fs.read("/dev/input/mouse0", Default::default());
     for foo in read.wait() {
-        println!("hooray: {:?}", foo);
+        let bar: Result<Bytes> = foo;
+        match bar {
+            Ok(v) => {
+                if v.len() != 3 {
+                    panic!("expected: length of 3");
+                }
+                let (x, y): (i8, i8) = unsafe { (transmute(v[1]), transmute(v[2])) };
+                print!("\rx: {:4}, y: {:4}", x, y);
+                let _ = stdout().flush();
+            }
+            Err(e) => panic!(e),
+        }
     }
 }
