f79830b9c115768d55e5e020d899ae004618339c
Sönke Hahn <soenkehahn@gmail.com>
2018-05-24 12:36:27 -0400

    convert jack client to stereo

--- src/main.rs
+++ src/main.rs
@@ -20,10 +20,16 @@ fn main_() -> Result<(), Error> {
     let (client, _status) =
         jack::Client::new("my-rust-client", jack::ClientOptions::NO_START_SERVER)?;
 
-    let port = client.register_port("mono-output", AudioOut)?;
+    let left_port = client.register_port("left-output", AudioOut)?;
+    let right_port = client.register_port("right-output", AudioOut)?;
 
     let notification_handler = ();
-    let process_handler = Generator { port: port };
+    let process_handler = Generator {
+        ports: Stereo {
+            left: left_port,
+            right: right_port,
+        },
+    };
     let _active_client = client.activate_async(notification_handler, process_handler)?;
     sleep_forever();
     Ok(())
@@ -35,16 +41,24 @@ fn sleep_forever() {
     }
 }
 
+struct Stereo<Port> {
+    left: Port,
+    right: Port,
+}
+
 struct Generator {
-    port: Port<AudioOut>,
+    ports: Stereo<Port<AudioOut>>,
 }
 
 impl ProcessHandler for Generator {
     fn process(&mut self, _client: &Client, scope: &ProcessScope) -> Control {
         let mut rng = rand::thread_rng();
-        let buffer: &mut [f32] = self.port.as_mut_slice(scope);
-        for sample_index in 0..buffer.len() {
-            buffer[sample_index] = rng.gen_range(-1.0, 1.0) * 0.1;
+        let left_buffer: &mut [f32] = self.ports.left.as_mut_slice(scope);
+        let right_buffer: &mut [f32] = self.ports.right.as_mut_slice(scope);
+        for sample_index in 0..left_buffer.len() {
+            let sample = rng.gen_range(-1.0, 1.0) * 0.1;
+            left_buffer[sample_index] = sample;
+            right_buffer[sample_index] = sample;
         }
         Control::Continue
     }
