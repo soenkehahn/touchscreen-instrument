17e5e28451eb42b1173badd94c79a9ce31fd6b3a
Sönke Hahn <soenkehahn@gmail.com>
2018-06-20 18:10:49 -0400

    support parallelograms

--- src/areas/shape.rs
+++ src/areas/shape.rs
@@ -15,6 +15,11 @@ pub enum Shape {
         b: Position,
         c: Position,
     },
+    Parallelogram {
+        base: Position,
+        u: Position,
+        v: Position,
+    },
 }
 
 impl Shape {
@@ -42,6 +47,18 @@ impl Shape {
                 let abc_area = Shape::triangle_area(a, b, c);
                 abp_area + bcp_area + cap_area == abc_area
             }
+            Shape::Parallelogram { base, u, v } => {
+                let translated_position = Position {
+                    x: position.x - base.x,
+                    y: position.y - base.y,
+                };
+                let multiplication_factor = 1.0 / (u.x * v.y - v.x * u.y) as f32;
+                let u_component = (translated_position.x * v.y + translated_position.y * (-v.x))
+                    as f32 * multiplication_factor;
+                let v_component = (translated_position.x * (-u.y) + translated_position.y * u.x)
+                    as f32 * multiplication_factor;
+                u_component >= 0.0 && u_component <= 1.0 && v_component >= 0.0 && v_component <= 1.0
+            }
         }
     }
 
@@ -63,6 +80,10 @@ impl Shape {
             Shape::Triangle { a, b, c } => (
                 Box::new([a.x as i16, b.x as i16, c.x as i16]),
                 Box::new([a.y as i16, b.y as i16, c.y as i16]),
+            ),
+            Shape::Parallelogram { base, u, v } => (
+                Box::new([base.x as i16, u.x as i16, (u.x + v.x) as i16, v.x as i16]),
+                Box::new([base.y as i16, u.y as i16, (u.y + v.y) as i16, v.y as i16]),
             ),
         };
         for x in xs.iter_mut() {
@@ -186,6 +207,52 @@ mod test {
                 assert!(!triangle.contains(Position { x: 1, y: 9 }));
             }
         }
+
+        mod parallelogram {
+            use super::*;
+
+            const PARALLELOGRAM: Shape = Shape::Parallelogram {
+                base: Position { x: 0, y: 0 },
+                u: Position { x: 10, y: 5 },
+                v: Position { x: 5, y: 10 },
+            };
+
+            #[test]
+            fn detects_positions_inside() {
+                assert!(PARALLELOGRAM.contains(Position { x: 5, y: 5 }));
+                assert!(PARALLELOGRAM.contains(Position { x: 10, y: 10 }));
+                assert!(PARALLELOGRAM.contains(Position { x: 5, y: 9 }));
+            }
+
+            #[test]
+            fn returns_true_for_corners() {
+                assert!(PARALLELOGRAM.contains(Position { x: 0, y: 0 }));
+                assert!(PARALLELOGRAM.contains(Position { x: 5, y: 10 }));
+                assert!(PARALLELOGRAM.contains(Position { x: 15, y: 15 }));
+                assert!(PARALLELOGRAM.contains(Position { x: 10, y: 5 }));
+            }
+
+            #[test]
+            fn detects_positions_outside() {
+                assert!(!PARALLELOGRAM.contains(Position { x: 2, y: 8 }));
+                assert!(!PARALLELOGRAM.contains(Position { x: 7, y: 13 }));
+                assert!(!PARALLELOGRAM.contains(Position { x: 13, y: 7 }));
+                assert!(!PARALLELOGRAM.contains(Position { x: 8, y: 2 }));
+            }
+
+            #[test]
+            fn works_for_translated_parallelograms() {
+                let parallelogram = Shape::Parallelogram {
+                    base: Position { x: 10, y: 10 },
+                    u: Position { x: 10, y: 0 },
+                    v: Position { x: 0, y: 10 },
+                };
+                assert!(!parallelogram.contains(Position { x: 5, y: 5 }));
+                assert!(!parallelogram.contains(Position { x: 15, y: 5 }));
+                assert!(!parallelogram.contains(Position { x: 5, y: 15 }));
+                assert!(parallelogram.contains(Position { x: 15, y: 15 }));
+            }
+        }
     }
 
     mod to_polygon {
@@ -216,6 +283,35 @@ mod test {
                 let expected: (Box<[i16]>, Box<[i16]>) =
                     (Box::new([2, 20, 10]), Box::new([3, 30, 30]));
                 assert_eq!(triangle.to_polygon(2.0, 3.0), expected);
+            }
+        }
+
+        mod parallelogram {
+            use super::*;
+
+            #[test]
+            fn converts_correctly() {
+                let parallelogram = Shape::Parallelogram {
+                    base: Position { x: 0, y: 0 },
+                    u: Position { x: 10, y: 5 },
+                    v: Position { x: 5, y: 10 },
+                };
+
+                let expected: (Box<[i16]>, Box<[i16]>) =
+                    (Box::new([0, 10, 15, 5]), Box::new([0, 5, 15, 10]));
+                assert_eq!(parallelogram.to_polygon(1.0, 1.0), expected);
+            }
+
+            #[test]
+            fn converts_touch_coordinates_correctly() {
+                let parallelogram = Shape::Parallelogram {
+                    base: Position { x: 0, y: 0 },
+                    u: Position { x: 1, y: 0 },
+                    v: Position { x: 0, y: 1 },
+                };
+                let expected: (Box<[i16]>, Box<[i16]>) =
+                    (Box::new([0, 2, 2, 0]), Box::new([0, 0, 3, 3]));
+                assert_eq!(parallelogram.to_polygon(2.0, 3.0), expected);
             }
         }
 
