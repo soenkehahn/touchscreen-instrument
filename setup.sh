e3e6342495373298419540156076e60a633ec9cb
Caroline Lin <ruoyi.lin@gmail.com>
2018-07-29 19:07:08 -0400

    sorry

--- setup.sh
+++ setup.sh
@@ -11,11 +11,11 @@ xinit $RUST_APP --layout Parallelograms &
 sleep 3 
 
 # connect the clients 
-SYSTEM_LEFT= "system:playback_1"
-SYSTEM_RIGHT= "system:playback_2"
+SYSTEM_LEFT="system:playback_1"
+SYSTEM_RIGHT="system:playback_2"
 
-RUST_LEFT= "rust-device-reading:left-output"
-RUST_RIGHT= "rust-device-reading:right-output"
+RUST_LEFT="rust-device-reading:left-output"
+RUST_RIGHT="rust-device-reading:right-output"
 
 jack_connect $SYSTEM_LEFT $RUST_LEFT
 jack_connect $SYSTEM_RIGHT $RUST_RIGHT
