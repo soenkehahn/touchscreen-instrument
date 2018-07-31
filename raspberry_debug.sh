94b3af29bb55a356b5a3accafb326913ae06baa8
Caroline Lin <ruoyi.lin@gmail.com>
2018-07-30 22:17:49 -0400

    Add Makefile target for debugging on raspi (#24)
    * Add Makefile target for debugging on raspi
    * build executable in release mode
    * Update raspberry_debug.sh

---
+++ raspberry_debug.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+# from https://wiki.linuxaudio.org/wiki/raspberrypi
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket
+
+xinit $PWD/target/release/rust-device-reading \
+    --layout Parallelograms \
+    --volume 5 &
+sleep 3
+
+jack_connect "system:playback_1" "rust-device-reading:left-output"
+jack_connect "system:playback_2" "rust-device-reading:right-output"
