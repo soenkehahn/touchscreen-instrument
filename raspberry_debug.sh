f994313bb1bf17d9e8fa949268ee2453311bbf0c
soenkehahn <SoenkeHahn@gmail.com>
2018-07-30 23:45:06 -0400

    wait longer for instrument to be started before connecting jack ports

--- raspberry_debug.sh
+++ raspberry_debug.sh
@@ -8,7 +8,7 @@ export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket
 xinit $PWD/target/release/rust-device-reading \
     --layout Parallelograms \
     --volume 5 &
-sleep 3
+sleep 5
 
 jack_connect "system:playback_1" "rust-device-reading:left-output"
 jack_connect "system:playback_2" "rust-device-reading:right-output"
