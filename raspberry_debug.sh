d3cdfa879d4b9b4ecaac3e10422974890473b7ae
Sönke Hahn <soenkehahn@gmail.com>
2018-09-01 12:39:29 -0400

    rename project (#29)

--- raspberry_debug.sh
+++ raspberry_debug.sh
@@ -5,6 +5,6 @@ set -o errexit
 # from https://wiki.linuxaudio.org/wiki/raspberrypi
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket
 
-xinit $PWD/target/release/rust-device-reading \
+xinit $PWD/target/release/touchscreen-instrument \
     --layout Parallelograms \
     --volume 5
