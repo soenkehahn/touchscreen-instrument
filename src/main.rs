40dcbe644b19c26e470280e49c0717e315556773
Sönke Hahn <soenkehahn@gmail.com>
2018-10-26 11:47:23 -0400

    add flipped layout (#56)
    * add flipped layout
    
    * tweak configuration for flipped layout
    
    * rename layout to Grid

--- src/main.rs
+++ src/main.rs
@@ -110,7 +110,7 @@ custom_derive! {
 #[derive(Debug, Clone, Copy, IterVariants(LayoutTypeVariants), PartialEq)]
     pub enum LayoutType {
         Parallelograms,
-        Dummy,
+        Grid,
     }
 }
 
@@ -130,7 +130,7 @@ fn get_areas(layout_type: &LayoutType) -> Areas {
             24,
             5,
         ),
-        LayoutType::Dummy => get_areas(&LayoutType::Parallelograms),
+        LayoutType::Grid => Areas::grid(TOUCH_WIDTH as i32, TOUCH_HEIGHT as i32, 16, 11, 36),
     }
 }
 
